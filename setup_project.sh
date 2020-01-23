b21661b01c897762c6c96f5a4bc4ce8ccdb35913
Steve Shaw <steve.shaw@gov.bc.ca>
2020-01-23 13:33:57 -0800

    Moving setup scripts to bcgov cap URLs

--- setup_project.sh
+++ setup_project.sh
@@ -1,2 +1,2 @@
-curl -LJO https://raw.githubusercontent.com/daveram/cap-eagle-dev-guides/master/project_installer.sh;
+curl -LJO https://raw.githubusercontent.com/bcgov/cap-eagle-dev-guides/master/project_installer.sh;
 source ./project_installer.sh;
